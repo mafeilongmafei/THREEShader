e740cede28fab46572e491a49dff1a8176ee62c7
mfl <2216913891.com>
2022-08-09 16:39:52 +0800

    2

--- js/shader.js
+++ js/shader.js
@@ -124,3 +124,104 @@ const material = new THREE.ShaderMaterial({
     side: THREE.DoubleSide
 
 });
+
+
+
+
+
+// =========================================================
+
+
+
+
+
+
+
+
+const wallData = [{
+    position: {
+        x: -150,
+        y: 15,
+        z: 100
+    },
+    speed: 0.5,
+    color: '#efad35',
+    opacity: 0.6,
+    radius: 420,
+    height: 120,
+    renderOrder: 5
+},]
+
+
+
+const WallvertexShader = /*glsl*/`
+           uniform vec3 u_color;
+
+        uniform float time;
+        uniform float u_height;
+        
+        varying float v_opacity;
+
+        void main() {
+
+            vec3 vPosition = position * mod(time, 1.0);
+
+            v_opacity = mix(1.0, 0.0, position.y / u_height);
+        
+            gl_Position = projectionMatrix * modelViewMatrix * vec4(vPosition, 1.0);
+        }
+        
+        `
+const WallfragmentShader = /*glsl*/`
+                uniform vec3 u_color;
+                uniform float u_opacity;
+                
+                varying float v_opacity;
+
+                void main() { 
+                    gl_FragColor = vec4(u_color, v_opacity * u_opacity);
+                }
+                        
+        
+        `
+
+function wall(option = {}) {
+    const {
+        radius,
+        height,
+        opacity,
+        color,
+        speed,
+        renderOrder
+    } = option;
+    const geometry = new THREE.CylinderGeometry(radius, radius, height, 32, 1, true);
+    // geometry.translate(0, height / 2, 0);
+    const material = new THREE.ShaderMaterial({
+        uniforms: {
+            u_height: {
+                value: height
+            },
+            u_speed: {
+                value: speed || 1
+            },
+            u_opacity: {
+                value: opacity
+            },
+            u_color: {
+                value: new THREE.Color(color)
+            },
+            time: zTime
+        },
+        transparent: true,
+        depthWrite: false,
+        depthTest: false,
+        side: THREE.DoubleSide,
+        vertexShader: WallvertexShader,
+        fragmentShader: WallfragmentShader
+    });
+    const mesh = new THREE.Mesh(geometry, material);
+    mesh.renderOrder = renderOrder || 1;
+    return mesh;
+}
+
+
